fad29414ae71d3c7c14f40bffa30faf4b1877a36
DongHyeonLee <jsleeg7279@naver.com>
2022-09-14 20:22:50 +0900

    update show lena

--- week2/show_lena.py
+++ week2/show_lena.py
@@ -1,6 +1,27 @@
+import argparse
+
 import cv2
 
-img = cv2.imread('../images/lenna.png', 1)
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', default='../images/lenna.png', help='Image path.')
+params = parser.parse_args()
+
+
+img = cv2.imread(params.path)
+
+#Check if image was successfullly read.
+assert img is not None
+
+print(f'read {params.path}')
+print(f'shape: {img.shape}')
+print(f'dtype: {img.dtype}')
+
+img = cv2.imread(params.path, cv2.IMREAD_GRAYSCALE)
+assert img is not None
+print(f'read {params.path} as grayscale')
+print(f'shape: {img.shape}')
+print(f'dtype: {img.dtype}')
+
 cv2.imshow('Test', img)
 cv2.waitKey(0)
 
