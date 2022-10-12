d2a297aa2ffd0a4dd3e450938585b1254a559d04
Dong Hyeon Lee <jsleeg7279@naver.com>
2022-10-12 15:22:04 +0900

    task 2 수정

--- Task1/task_2_2.py
+++ Task1/task_2_2.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import animation, rc
 
 def on_diameter_change(pos):
     global bilat, diameter, SigmaColor, SigmaSpace
@@ -15,7 +14,7 @@ def on_diameter_change(pos):
 
 def on_SigmaColor_change(pos):
     global bilat, diameter, SigmaColor, SigmaSpace
-    SigmaColor = pos
+    SigmaColor = float(pos/10)
     print(f'diameter : {diameter}, SigmaColor : {SigmaColor}, SigmaSpace : {SigmaSpace} ')
     bilat = cv2.bilateralFilter(noised, diameter, SigmaColor, SigmaSpace)
     cv2.imshow('Result', bilat)
@@ -37,7 +36,7 @@ bilat = cv2.bilateralFilter(noised, diameter, SigmaColor, SigmaSpace)
 
 cv2.namedWindow('Result')
 cv2.createTrackbar('diameter', 'Result', 0, 10, on_diameter_change)
-cv2.createTrackbar('SigmaColor', 'Result', 0, 300, on_SigmaColor_change)
+cv2.createTrackbar('SigmaColor', 'Result', 0, 3000, on_SigmaColor_change)
 cv2.createTrackbar('SigmaSpace', 'Result', 0, 20, on_SigmaSpace_change)
 cv2.imshow('Result', bilat)
 cv2.waitKey()
