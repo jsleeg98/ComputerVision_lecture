4eec90cac8ca88d4a246fd65547a8a3bcab0b905
Dong Hyeon Lee <jsleeg7279@naver.com>
2022-10-25 19:50:40 +0900

    중간 프로젝트 - cartoon 추가

--- MidProject/project.py
+++ MidProject/project.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 from PIL import Image, ImageEnhance
 
-img = cv2.imread('test_9.jpg', cv2.IMREAD_COLOR)
-img = cv2.resize(img, (500, 600))
+img = cv2.imread('test_image_4.jpg', cv2.IMREAD_COLOR)
+# img = cv2.resize(img, (500, 600))
 show_img = np.copy(img)  # 영상 출력 이미지
 
 mouse_pressed = False
@@ -97,69 +97,77 @@ cv2.destroyAllWindows()
 mask_bg = np.where((labels == cv2.GC_PR_BGD) | (labels == cv2.GC_BGD), 1, 0).astype('uint8')  # 배경 마스크
 mask_obj = np.where((labels == cv2.GC_PR_BGD) | (labels == cv2.GC_BGD), 0, 1).astype('uint8')  # 물체 마스크
 def on_sigma_change(pos):  # trackbar를 이용하여 GaussianBlur sigma 조절
-    global result, mask_bg, obj_final, bg_blur, img
+    global result_outfocus, mask_bg, obj, bg_blur, img
     sigma = pos
     if sigma == 0:  # sigma가 0인 경우 1로 바꾸어 오류 제거
         sigma = 1
     print(f'sigma : {sigma}')
     img_blur = cv2.GaussianBlur(img, (0, 0), sigma)
     bg_blur = img_blur * mask_bg[:, :, np.newaxis]
-    result = bg_blur + obj_final
-    cv2.imshow('result', result)
-
-def on_kmean_change(pos):
-    global result, bg_blur, RGB_data, obj_final
-    if pos == 0:
-        num_classes = 128
-    elif pos == 1:
-        num_classes = 64
-    elif pos == 2:
-        num_classes = 32
-    elif pos == 3:
-        num_classes = 16
-    elif pos == 4:
-        num_classes = 8
-    elif pos == 5:
-        num_classes = 4
-    print(f'num_classes : {num_classes}')
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 50, 0.1)
-    _, labels, centers = cv2.kmeans(RGB_data, num_classes, None, criteria, 5, cv2.KMEANS_RANDOM_CENTERS)
-    obj_kmean = centers[labels.flatten()].reshape(img.shape).astype(np.uint8)
-    obj_final = obj_kmean * mask_obj[:, :, np.newaxis]
-    result = bg_blur + obj_final
-    cv2.imshow('result', result)
-
-def on_sigma_r_change(pos):
-
+    result_outfocus = bg_blur + obj
+    cv2.imshow('result_outfocus', result_outfocus)
 
 obj = img * mask_obj[:, :, np.newaxis]
-RGB_data = obj.reshape((-1, 3)).astype(np.float32)
-num_classes = 4
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 50, 0.1)
-_, labels, centers = cv2.kmeans(RGB_data, num_classes, None, criteria, 5, cv2.KMEANS_RANDOM_CENTERS)
-centers = np.uint8(centers)
-obj_kmean = centers[labels.flatten()].reshape(img.shape)
-
-obj_final = obj_kmean * mask_obj[:, :, np.newaxis]
 img_blur = cv2.GaussianBlur(img, (0, 0), 1)
 bg_blur = img_blur * mask_bg[:, :, np.newaxis]
-result = bg_blur + obj_final
+result_outfocus = bg_blur + obj
 
-cv2.namedWindow('result')
-cv2.createTrackbar('sigma', 'result', 1, 20, on_sigma_change)
-# cv2.createTrackbar('kmeans', 'result', 5, 5, on_kmean_change)
-cv2.createTrackbar('sigma_r', 'result', 1, 10, on_sigma_r_change)
+cv2.namedWindow('result_outfocus')
+cv2.createTrackbar('sigma', 'result_outfocus', 1, 20, on_sigma_change)
+
+
+
 while True:
-    cv2.imshow('result', result)
+    cv2.imshow('result_outfocus', result_outfocus)
     cv2.imshow('original', img)
     k = cv2.waitKey(1)
     if k == ord('s'):  # 현재 이미지 저장
-        cv2.imwrite('result.jpg', result)
+        cv2.imwrite('result_outfocus.jpg', result_outfocus)
 
-    elif k == ord('n'):  # 종료
+    elif k == ord('n'):  # 다음 단계
         break
 cv2.destroyAllWindows()
 
+def on_sigma_s_change(pos):
+    global result_cartoon, bg_blur, obj, img, sigma_s, sigma_r, mask_obj
+    sigma_s = pos
+    print(f'sigma_r : {sigma_r}')
+    print(f'sigma_s : {sigma_s}')
+    img_cartoon = cv2.stylization(img, sigma_s=sigma_s, sigma_r=sigma_r)
+    obj = img_cartoon * mask_obj[:, :, np.newaxis]
+    result_cartoon = bg + obj
+    cv2.imshow('result_cartoon', result_cartoon)
+def on_sigma_r_change(pos):
+    global result_cartoon, bg, obj, img, sigma_s, sigma_r, mask_obj
+    sigma_r = float(pos / 100)
+    print(f'sigma_r : {sigma_r}')
+    print(f'sigma_s : {sigma_s}')
+    img_cartoon = cv2.stylization(img, sigma_s=sigma_s, sigma_r=sigma_r)
+    obj = img_cartoon * mask_obj[:, :, np.newaxis]
+    result_cartoon = bg + obj
+    cv2.imshow('result_cartoon', result_cartoon)
 
+
+sigma_s = 100
+sigma_r = 0.25
+img_cartoon = cv2.stylization(img, sigma_s=sigma_s, sigma_r=sigma_r)
+obj = img_cartoon * mask_obj[:, :, np.newaxis]
+bg = img * mask_bg[:, :, np.newaxis]
+result_cartoon = obj + bg
+
+cv2.namedWindow('result_cartoon')
+cv2.createTrackbar('sigma_s', 'result_cartoon', 100, 200, on_sigma_s_change)
+cv2.createTrackbar('sigma_r', 'result_cartoon', 25, 100, on_sigma_r_change)
+
+while True:
+    cv2.imshow('result_cartoon', result_cartoon)
+    cv2.imshow('original', img)
+    k = cv2.waitKey(1)
+    if k == ord('s'):  # 현재 이미지 저장
+        cv2.imwrite('result_cartoon.jpg', result_cartoon)
+
+    elif k == ord('n'):  # 다음 단계
+        break
+cv2.destroyAllWindows()
 
 
